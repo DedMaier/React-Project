0a94cc8c95e9b5bbe36d850e1f56c5500500214e
Вячеслав Хохлов <slava071172@mail.ru>
2022-12-19 14:00:21 +0300

    kommit

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -3,13 +3,16 @@ import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = (props) => {
+  let posts = [
+    { id: 1, message: "Добрый день!", likesCount: 0 },
+    { id: 2, message: "Здравствуйте!", likesCount: 10 },
+    { id: 3, message: "Привет, как ты?", likesCount: 8 },
+    { id: 4, message: "It's my first post", likesCount: 12 },
+  ];
 
-  let postData = [
-    {id: 1, message: 'Добрый день!', likesCount: 0},
-    {id: 2, message: 'Здравствуйте!', likesCount: 10},
-    {id: 3, message: 'Привет, как ты?', likesCount: 8},
-    {id: 4, message: "It's my first post", likesCount: 12},
-  ]
+  let postsElements = posts.map((p) => (
+    <Post message={p.message} likesCount={p.likesCount} />
+  ));
 
   return (
     <div className={s.postBlock}>
@@ -22,12 +25,8 @@ const MyPosts = (props) => {
           <button>Add post</button>
         </div>
       </div>
-      <div className={s.posts}>
-        <Post message={postData[0].message} likesCount={postData[0].likesCount}/>
-        <Post message={postData[1].message} likesCount={postData[1].likesCount} />
-        <Post message={postData[2].message} likesCount={postData[2].likesCount} />
-        <Post message={postData[3].message} likesCount={postData[3].likesCount} />
-      </div>
+      <div className={s.posts}></div>
+      {postsElements}
     </div>
   );
 };
